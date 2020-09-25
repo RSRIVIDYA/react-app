cc5d931f239f8c3de3343585636f5b3aedcc57f6
srividya <srividyatech31@gmail.com>
2020-09-25 22:21:08 +0530

    Updates

---
+++ precache-manifest.719a6ff03729b3e200ef3e018e54aba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7f513e489cf4b1c132dcb74ae686c8",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ad6036d9a51ce5ebd7a2",
+    "url": "/react-app/static/css/2.70891016.chunk.css"
+  },
+  {
+    "revision": "6ee8e543e324c4c76117",
+    "url": "/react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad6036d9a51ce5ebd7a2",
+    "url": "/react-app/static/js/2.2507343a.chunk.js"
+  },
+  {
+    "revision": "aba56185c4239e8868e235db4b129b2d",
+    "url": "/react-app/static/js/2.2507343a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee8e543e324c4c76117",
+    "url": "/react-app/static/js/main.a427eb51.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
